83f31941f67bb74142c98af3debd02e652cd84a1
questmapping <code@alessiovmagri.com>
2019-06-15 16:40:34 +0000

    Aggiunta la cartella per la gestione DB e connessioni e preparato l'environment di sviluppo su cloud9

--- connessioni/connect.php
+++ connessioni/connect.php
@@ -1,16 +1,14 @@
 <?php
 //Connette al database
 
-$dbHost=getenv('IP');
-$dbUser=getenv('C9_USER');
+$dbHost="localhost";
+$dbUser="root";
 $dbPass="password";
-$dbport = 3306;
-$connect=mysql_connect($dbHost, $dbUser, $dbPass) or die ('Could not connect to a MySQL server using the default settings');
-
-
 $dbVn="mporio_db";
+$dbport = 3306;
 
-$temp=mysql_select_db($dbVn, $connect) or die ('Cannot select database');
+$connect=mysqli_connect($dbHost, $dbUser, $dbPass,$dbVn) or die ('Could not connect to a MySQL server using the default settings');
+
 
 
 ?>
